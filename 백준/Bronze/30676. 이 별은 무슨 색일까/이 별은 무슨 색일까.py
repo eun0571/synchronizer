c5f5ba44406200fdee329a40b89b6eeb204e49bf
eun0571 <eun0571@gmail.com>
2024-08-15 22:05:17 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,18 @@
+import sys
+
+a = int(sys.stdin.readline())
+
+if a >= 620:
+    print("Red")
+elif a >= 590:
+    print("Orange")
+elif a >= 570:
+    print("Yellow")
+elif a >= 495:
+    print("Green")
+elif a >= 450:
+    print("Blue")
+elif a >= 425:
+    print("Indigo")
+else:
+    print("Violet")
