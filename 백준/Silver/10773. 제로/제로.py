369d17ec1ed86fd5518fd9e39ea91e864da8d83b
eun0571 <eun0571@gmail.com>
2024-08-16 13:04:06 +0900

    [Silver IV] Title: 제로, Time: 92 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+a = deque()
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    if num:
+        a.append(num)
+    else:
+        a.pop()
+
+print(sum(a))
