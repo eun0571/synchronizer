104f59e960b6a57d299ca92bc23ef11e729fcb12
eun0571 <eun0571@gmail.com>
2024-08-18 12:29:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 164 ms, Memory: 40332 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -9,7 +9,7 @@ for i in range(m):
     x, y = map(int, sys.stdin.readline().split())
     matrix[x - 1][y - 1] = 1
     matrix[y - 1][x - 1] = 1
-##########################################################
+# DFS 재귀로 구현하기
 visited = [0 for i in range(n)]
 
 
@@ -22,10 +22,30 @@ def DFS(v):
 
 
 visited[v - 1] = 1
-DFS(v - 1)
+# DFS(v - 1)
+# DFS 스택으로 구현하기
+visited = [0 for i in range(n)]
+stack = deque()
+
+
+def DFS_stack(v):
+    stack.append(v)
+    while stack:
+        a = stack.pop()
+        if visited[a] == 0:
+            visited[a] = 1
+            print(a + 1, end=" ")
+            # 작은 번호의 정점을 먼저 처리하기 위해 정순으로 스택에 넣음
+            for i in range(n - 1, -1, -1):
+                if matrix[a][i] and not visited[i]:
+                    stack.append(i)
+
+
+DFS_stack(v - 1)
+print()  # 줄바꿈
 ##########################################################
-print()
-##########################################################
+# BFS 구현하기
+
 visited = [0 for i in range(n)]
 q = deque()
 
@@ -33,7 +53,7 @@ q = deque()
 def BFS(v):
     q.append(v)
     visited[v] = 1
-    while len(q):
+    while q:
         a = q.popleft()
         print(a + 1, end=" ")
         for i in range(n):
