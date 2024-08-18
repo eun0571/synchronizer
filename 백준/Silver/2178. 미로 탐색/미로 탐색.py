e68d881622be1904758ab6184e101196d8a63bec
eun0571 <eun0571@gmail.com>
2024-08-19 00:42:15 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+
+matrix = [[] for j in range(n)]
+
+for i in range(n):
+    for j in sys.stdin.readline().strip():
+        matrix[i].append(int(j))
+
+q = deque()
+
+navi = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+def maze(x, y):
+    q.append((x, y, 1))
+    matrix[x][y] = 0
+    candy = []
+    while q:
+        a = q.popleft()
+        if (a[0], a[1]) == (n - 1, m - 1):
+            candy.append(a[2])
+        x = a[0]
+        y = a[1]
+        for i in range(4):
+            X = x + navi[i][0]
+            Y = y + navi[i][1]
+
+            if -1 < X < n and -1 < Y < m and matrix[X][Y]:
+                matrix[X][Y] = 0
+                q.append((X, Y, a[2] + 1))
+    return min(candy)
+
+
+print(maze(0, 0))
