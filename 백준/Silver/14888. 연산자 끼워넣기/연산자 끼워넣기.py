67fb7a9ec76658fb12bdf023c8e049e83791a45a
eun0571 <eun0571@gmail.com>
2024-08-20 21:39:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1568 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,34 +1,29 @@
 import sys
 
-N = int(sys.stdin.readline())
-
+m = int(sys.stdin.readline())
 l = list(map(int, sys.stdin.readline().split()))
+op = ["+", "-", "*", "//"]
+remain = list(map(int, sys.stdin.readline().split()))
 
-op = list(map(int, sys.stdin.readline().split()))
-
-op_set = ["+", "-", "*", "//"]
-
-used_op = [0, 0, 0, 0]
+result = []
 
 
-# 뒤에서부터 연산자 놓는다고 생각
-def insert_op(n: int):  # 사용 가능한 연산자 개수, 앞의 연산 결과값
-    if n == 0:
+def DFS(n):
+    if n < 1:
         return [l[0]]
-    else:
-        answer = []
-        for i in range(4):
-            if used_op[i] < op[i]:
-                used_op[i] += 1
-                prev_result = insert_op(n - 1)
-                for j in prev_result:
-                    if j < 0 and i == 3:
-                        answer.append(eval(str(abs(j)) + op_set[i] + str(l[n])) * (-1))
-                    else:
-                        answer.append(eval(str(j) + op_set[i] + str(l[n])))
-                used_op[i] -= 1
+    answer = []
+    for i in range(4):
+        if remain[i]:
+            remain[i] -= 1
+            for j in DFS(n - 1):
+                if j < 0 and i == 3:
+                    answer.append(-eval(f"{-j}{op[i]}{l[n]}"))
+                else:
+                    answer.append(eval(f"{j}{op[i]}{l[n]}"))
+            remain[i] += 1
     return answer
 
 
-print(sorted(insert_op(N - 1))[-1])
-print(sorted(insert_op(N - 1))[0])
+result = DFS(m - 1)
+print(max(result))
+print(min(result))
