6b0692b9975cfa5cadd613177425eac525745cc0
eun0571 <eun0571@gmail.com>
2024-08-22 02:22:25 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -3,64 +3,42 @@ from collections import deque
 
 n, m, v = map(int, sys.stdin.readline().split())
 
-matrix = [[0 for i in range(n)] for j in range(n)]
-
+e = [[] for i in range(n)]
 for i in range(m):
-    x, y = map(int, sys.stdin.readline().split())
-    matrix[x - 1][y - 1] = 1
-    matrix[y - 1][x - 1] = 1
-# DFS 재귀로 구현하기
-visited = [0 for i in range(n)]
+    a, b = map(int, sys.stdin.readline().split())
+    e[a - 1].append(b - 1)
+    e[b - 1].append(a - 1)
+
+for i in range(n):
+    e[i].sort()
+
+visited = [0 for _ in range(n)]
 
 
-def DFS(v):
+def dfs(e, v):
     print(v + 1, end=" ")
-    for i in range(n):
-        if matrix[v][i] and not visited[i]:
+    for i in e[v]:
+        if not visited[i]:
             visited[i] = 1
-            DFS(i)
+            dfs(e, i)
 
 
 visited[v - 1] = 1
-# DFS(v - 1)
-# DFS 스택으로 구현하기
-visited = [0 for i in range(n)]
-stack = deque()
+dfs(e, v - 1)
+print()
 
 
-def DFS_stack(v):
-    stack.append(v)
-    while stack:
-        a = stack.pop()
-        if visited[a] == 0:
-            visited[a] = 1
-            print(a + 1, end=" ")
-            # 작은 번호의 정점을 먼저 처리하기 위해 정순으로 스택에 넣음
-            for i in range(n - 1, -1, -1):
-                if matrix[a][i] and not visited[i]:
-                    stack.append(i)
-
-
-DFS_stack(v - 1)
-print()  # 줄바꿈
-##########################################################
-# BFS 구현하기
-
-visited = [0 for i in range(n)]
-q = deque()
-
-
-def BFS(v):
-    q.append(v)
-    visited[v] = 1
+def bfs():
     while q:
         a = q.popleft()
         print(a + 1, end=" ")
-        for i in range(n):
-            if matrix[a][i] and not visited[i]:
+        for i in e[a]:
+            if not visited[i]:
                 visited[i] = 1
                 q.append(i)
 
 
+visited = [0 for _ in range(n)]
 visited[v - 1] = 1
-BFS(v - 1)
+q = deque([v - 1])
+bfs()
