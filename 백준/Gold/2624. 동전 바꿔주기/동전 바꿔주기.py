dbdf33008f343dba4632767e8e87e92d6293bb25
eun0571 <eun0571@gmail.com>
2024-08-23 21:35:30 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 420 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -14,7 +14,9 @@ dp = [0 for _ in range(t + 1)]
 dp[0] = 1
 for coin, num in coins:
     for i in range(t, 0, -1):
-        for k in range(num, 0, -1):
-            if i - k * coin >= 0 and dp[i - k * coin]:
+        for k in range(1, num+1):
+            if i - k * coin >= 0:
                 dp[i] += dp[i - k * coin]
+            else:
+                break
 print(dp[t])
