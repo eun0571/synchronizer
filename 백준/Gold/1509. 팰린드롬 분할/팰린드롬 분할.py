bec06ee0b0c13c82d0dec5c26637f6baf9775325
eun0571 <eun0571@gmail.com>
2024-08-24 19:43:33 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 2212 ms, Memory: 80572 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
@@ -2,29 +2,25 @@ import sys
 
 l = list(sys.stdin.readline().strip())
 n = len(l)
-
-dp = [[0 if i != j else 1 for i in range(n + 1)] for j in range(n + 1)]
+dp = [[0 if i != j else 1 for i in range(n)] for j in range(n)]
 
 for i in range(1, n):
     if l[i - 1] == l[i]:
-        dp[i][i + 1] = 1
-# 길이 3이상
+        dp[i - 1][i] = 1
 for length in range(3, n + 1):
-    for i in range(n - length + 2):
-        j = i + length - 1
-        if l[i - 1] == l[j - 1]:
-            if dp[i + 1][j - 1] or (dp[i + 1][j] and dp[i][j - 1]):
-                dp[i][j] = 1
+    for i in range(length - 1, n):
+        j = i - length + 1
+        if l[i] == l[j] and dp[j + 1][i - 1]:
+            dp[j][i] = 1
 
-min_cuts = [float("inf")] * n
-
-for i in range(1, n + 1):
-    if dp[1][i]:  # 처음부터 i까지가 팰린드롬인 경우
-        min_cuts[i - 1] = 0
+min_cut = [sys.maxsize for _ in range(n)]
+min_cut[0] = 0
+for i in range(n):
+    if dp[0][i]:
+        min_cut[i] = 0
     else:
-        for j in range(1, i):
+        for j in range(i):
             if dp[j + 1][i]:
-                min_cuts[i - 1] = min(min_cuts[i - 1], min_cuts[j - 1] + 1)
+                min_cut[i] = min(min_cut[i], min_cut[j] + 1)
 
-# 결과 출력
-print(min_cuts[-1] + 1)
+print(min_cut[-1] + 1)
