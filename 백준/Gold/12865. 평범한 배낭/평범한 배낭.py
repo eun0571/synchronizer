22a8feef1c7b585eaeef53837f6187d44d6aeac0
eun0571 <eun0571@gmail.com>
2024-08-25 22:20:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 1552 ms, Memory: 33804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -8,8 +8,8 @@ for _ in range(n):
 dp = [-1]*(k + 1)
 dp[0] = 0
 for w, v in goods:
-    for i in range(k, -1, -1):
-        if i - w >= 0 and dp[i - w] != -1:
+    for i in range(k, w-1, -1):
+        if dp[i - w] != -1:
             dp[i] = max(dp[i], dp[i - w] + v)
 
 print(max(dp))
