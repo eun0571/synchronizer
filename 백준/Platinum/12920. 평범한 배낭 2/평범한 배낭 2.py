d970b0dfac1186be7e747314063f5d323731a9d6
eun0571 <eun0571@gmail.com>
2024-08-25 23:46:19 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 184 ms, Memory: 110796 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.py
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.py
@@ -3,14 +3,13 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 dp = [0] * (m + 1)
 
-for _ in range(n):
-    weight, value, count = map(int, sys.stdin.readline().split())
-    i = 1
-    while count > 0:
-        actual_count = min(i, count)
-        count -= actual_count
-        for j in range(m, actual_count * weight - 1, -1):
-            dp[j] = max(dp[j], dp[j - actual_count * weight] + actual_count * value)
-        i *= 2
-
-print(max(dp))
+for i in range(n):
+    w, v, k = map(int, sys.stdin.readline().split())
+    a = 1
+    while k > 0:
+        count = min(a, k)
+        k -= count
+        for i in range(m, w * count - 1, -1):
+            dp[i] = max(dp[i - w * count] + v * count, dp[i])
+        a *= 2
+print(dp[-1])
