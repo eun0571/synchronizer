aa0ee9d974336471db3463c00fcb03ecf5e64a64
eun0571 <eun0571@gmail.com>
2024-08-25 22:15:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 2000 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,28 +1,15 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-w = [0]
-v = [0]
+goods = []
 for _ in range(n):
-    weight, value = map(int, sys.stdin.readline().split())
-    w.append(weight)
-    v.append(value)
-dp = [[None if j else 0 for j in range(k + 1)] for i in range(n + 1)]
+    goods.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        if j - w[i] >= 0:
-            if dp[i - 1][j - w[i]] != None and dp[i - 1][j] != None:
-                dp[i][j] = max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j])
-            elif dp[i - 1][j - w[i]] != None:
-                dp[i][j] = dp[i - 1][j - w[i]] + v[i]
-            elif dp[i - 1][j] != None:
-                dp[i][j] = dp[i - 1][j]
-        elif dp[i - 1][j] != None:
-            dp[i][j] = dp[i - 1][j]
+dp = [-1 for i in range(k + 1)]
+dp[0] = 0
+for w, v in goods:
+    for i in range(k, -1, -1):
+        if i - w >= 0 and dp[i - w] != -1:
+            dp[i] = max(dp[i], dp[i - w] + v)
 
-result = 0
-for i in range(1,k+1):
-    if dp[-1][i]:
-        result = max(result,dp[-1][i])
-print(result)
+print(max(dp))
