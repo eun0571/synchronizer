9d00c2e76ac76795b54086b97167f7f2dcf2334b
eun0571 <eun0571@gmail.com>
2024-08-26 11:30:43 +0900

    [Silver II] Title: 외판원 순회 2, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,31 +1,27 @@
 import sys
 
+graph = []
 N = int(sys.stdin.readline())
-matrix = []
-for i in range(N):
-    matrix.append(list(map(int, sys.stdin.readline().split())))
-visited = [0] * N
+v = 1
+c = sys.maxsize
+for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
 
-def TSP(n, start):  # 방문해야하는 도시 개수, 시작도시
+def tsp(n, c_city, c_cost, v):
+    global c
     if n == 1:
-        if matrix[start][0]:
-            visited[0] = 1
-            return [matrix[start][0]]
-        else:
-            return []
-    result = []
-    # 0(시작 도시)를 제외한 도시를 타겟
-    for i in range(1, N):
-        if matrix[start][i] and visited[i] == 0:
-            visited[i] = 1
-            reponse = TSP(n - 1, i)
-            if reponse:
-                for j in reponse:
-                    result.append(j + matrix[start][i])
-            visited[i] = 0
+        if graph[c_city][0]:
+            c = min(c, c_cost + graph[c_city][0])
+        return
+    if c_cost > c:
+        return
+    for n_city in range(1, N):
+        if not v & 1 << n_city and graph[c_city][n_city]:
+            tsp(n - 1, n_city, c_cost + graph[c_city][n_city], v | 1 << n_city)
 
-    return result
+    return
 
 
-print(sorted(TSP(N, 0))[0])
+tsp(N, 0, 0, v)
+print(c)
