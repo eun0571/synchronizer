9aa42f302845fbe6fb580f620bba6549361a2caf
eun0571 <eun0571@gmail.com>
2024-09-04 10:21:39 +0900

    [Silver III] Title: 최고의 피자, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+price_d, price_t = map(int, input().split())
+cal_d = int(input())
+cal_t = [int(input()) for _ in range(n)]
+cal_t.sort(reverse=True)
+topping = []
+for i in range(n):
+    cal = cal_d + sum(topping)
+    price = price_d + len(topping) * price_t
+    if (cal // price) <= (cal + cal_t[i]) // (price + price_t):
+        topping.append(cal_t[i])
+print((cal_d + sum(topping)) // (price_d + len(topping) * price_t))
