5eb3b099456cb10b9baf9dea83ce670868dc5426
eun0571 <eun0571@gmail.com>
2024-10-23 15:47:01 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -2,11 +2,11 @@ import sys
 
 n = int(sys.stdin.readline())
 result = []
-for i in range(2, n + 1):
+for i in range(2, int(n**0.5) + 1):
     while n % i == 0:
-        result.append(i)
+        print(i)
         n //= i
     if n == 1:
         break
-for i in result:
-    print(i)
+if n != 1:
+    print(n)
